85f25c943494f70929ecd2dfd1b5058c5c3314b1
Jean-Pierre Reis <68484174+Jean-Pierre-Reis@users.noreply.github.com>
2024-12-28 23:51:45 -0300

    Update Display.cpp
    Implemented methods.

--- Display.cpp
+++ Display.cpp
@@ -8,5 +8,28 @@ public:
 
 private:
     MatrixPanel_I2S_DMA* dma_display;
-    // Outros atributos e métodos de display...
 };
+
+void Display::initialize() {
+    dma_display = new MatrixPanel_I2S_DMA();
+    dma_display->begin();
+    dma_display->clearScreen();
+}
+
+void Display::drawM(int x, int y) {
+    dma_display->drawPixel(x, y, dma_display->color565(0, 255, 0)); // Desenhar um pixel verde
+}
+
+void Display::drawT(int x, int y) {
+    dma_display->drawPixel(x, y, dma_display->color565(255, 0, 0)); // Desenhar um pixel vermelho
+}
+
+void Display::clearScreen() {
+    dma_display->clearScreen();
+}
+
+void Display::drawText(int x, int y, const char* text) {
+    dma_display->setCursor(x, y);
+    dma_display->setTextColor(dma_display->color565(255, 255, 255));
+    dma_display->print(text);
+}
